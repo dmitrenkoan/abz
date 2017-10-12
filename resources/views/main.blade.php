05f15d606bd487671690dff3beffe747020f4772
dmitrenkoan <dmitrenkoan90@gmail.com>
2017-10-12 13:18:05 +0300

    add new title

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -57,6 +57,7 @@
     <div class="starter-template">
         <h1>Bootstrap starter template</h1>
         <h2>Test message</h2>
+        <h3>Test title 3</h3>
         <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
     </div>
 
